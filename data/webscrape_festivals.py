d4df3b0f62972c2e0b8829698d88cbbd7cfe1e7f
Victor Thai <victhaaa@Victors-MacBook-Pro.local>
2024-11-27 13:38:50 -0800

    update webscraping code for US festivals

--- data/webscrape_festivals.py
+++ data/webscrape_festivals.py
@@ -2,39 +2,47 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 import json
 
 # create a function that uses a client scraper to scrape through the webpage
 def webscrape_all_festivals(festival_url):
-    """runs through all pages of musicfestivalwizard to obtain all festivals"""
-    all_festivals = {}
+    """runs through all pages of musicfestivalwizard to obtain all US festivals"""
+    all_festivals = {'festivals':{}}
     page_count = 1
 
     # first page has diff url
     driver = webdriver.Chrome()
-    festival_url = "https://www.musicfestivalwizard.com/all-festivals/"
+    festival_url = "https://www.musicfestivalwizard.com/festival-guide/us-festivals/"
     driver.get(festival_url)
 
     # loop until a page without page numbers
     while True and len(driver.find_elements(By.CLASS_NAME, "page-numbers")) != 0: 
         if page_count != 1:
-            festival_url = f"https://www.musicfestivalwizard.com/all-festivals/page/{page_count}/"
+            festival_url = f"https://www.musicfestivalwizard.com/festival-guide/us-festivals/page/{page_count}/"
         driver = webdriver.Chrome()
         driver.get(festival_url)
+
+        # pause to allow loading
+        time.sleep(1)
         elements = driver.find_elements(By.CLASS_NAME, "entry-title.search-title")
         for element in elements:
-            if len(element.text) <= 1:
+            if len(element.text) < 1:
                 continue
             else:
-                all_festivals[element.text] = 1
+                festival_info = str(element.text).split('\n')
+                try:
+                    if_cancelled = lambda x: True if 'CANCELLED' in x else False
+                    is_cancelled = if_cancelled(festival_info[2])
+                    all_festivals['festivals'][festival_info[0]] = {'location': festival_info[1], 'dates': festival_info[2], 'cancelled': is_cancelled}
+                except:
+                    continue
         page_count += 1
         time.sleep(3)
     return all_festivals
 
 # call function and add to json file
-all_festival_output = webscrape_all_festivals("https://www.musicfestivalwizard.com/all-festivals/")
+all_festival_output = webscrape_all_festivals("https://www.musicfestivalwizard.com/festival-guide/us-festivals/")
 
 with open('data/all_festivals.json', 'w') as output_file:
     json.dump(all_festival_output, output_file)
