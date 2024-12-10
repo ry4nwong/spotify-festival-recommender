156a1499d4551e8bc449190e688a3d547419173d
Ryan Wong <r3wong@ucsd.edu>
2024-12-09 19:52:35 -0800

    Ryan's work for now

--- backend/app/models/artist.py
+++ backend/app/models/artist.py
@@ -1,1 +1,12 @@
 # Artist: id, name (string), genre (list for now)
+from app import db
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    genres = db.relationship(
+        'Tag',
+        secondary='artist_tags',
+        backref=db.backref('artists', lazy='dynamic')
+    )
--- backend/app/models/festival.py
+++ backend/app/models/festival.py
@@ -1,8 +1,19 @@
 from app import db
 
-# Still needs artists (list), tags (list)
 class Festival(db.Model):
+    __tablename__ = 'festivals'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(255), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.String(255), nullable=False)
+    cancelled = db.Column(db.Boolean, default=False)
+    artists = db.relationship(
+        'Artist',
+        secondary=festival_artists,
+        backref=db.backref('festivals', lazy='dynamic')
+    )
+    tags = db.relationship(
+        'Tag',
+        secondary=festival_tags,
+        backref=db.backref('festivals', lazy='dynamic')
+    )
